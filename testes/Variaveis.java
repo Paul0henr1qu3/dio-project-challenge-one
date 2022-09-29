d67379c00c93fad25f311c3f8411b840caf7ce59
Paul0Henr1qu3 <paulhenriqueoliveira@gmail.com>
2022-09-28 22:03:40 -0300

    Criando variaveis

---
+++ testes/Variaveis.java
@@ -0,0 +1,15 @@
+public class Variaveis {
+  public static void main(String[] args) {
+    
+    int i;
+    final int j = 10;
+    int quantidadeProdutos = 50;
+    i = 10;
+
+    System.out.println(i);
+    System.out.println(quantidadeProdutos);
+    System.out.println(j);
+  
+  }
+  
+}
